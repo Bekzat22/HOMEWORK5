e6dd99d477d0c89ae51b28e3b84a0a1cc78d73c1
Bekzat22 <ibila.bekzat@bk.ru>
2023-01-13 18:13:49 +0600

    S

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 //        ● В классе Main создать 1 экземпляр босса и задать ему все свойства (значения
 //        полям). Затем распечатать всю информацию о боссе.
+//ДЗ на сообразительность:
+//        ● Добавить метод в классе Main который называется createHeroes, в теле метода
+//        необходимо создать 3х героев с помощью класса Hero, используя при этом разные
+//        варианты конструкторов (либо первый либо второй). Затем поместить созданные
+//        экземпляры героев в массив и вернуть его с помощью команды return, как
+//        возвращаемый результат метода createHeroes.
+//        ● Затем вызвать этот метод в методе main и распечатать информацию о всех героях
+//        массива через цикл.
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +17,40 @@ public class Main {
         boss.setDamage(20);
         boss.setQuirk("Barrier");
         System.out.println(boss.getHealth() +" "+ boss.getDamage()+" "+boss.getQuirk());
+
+        for (Hero heroes: createHeroes()) {
+            if(heroes.getQuirk()==null){
+                System.out.println(heroes.getHealth()+ " "+heroes.getDamage());
+            }else {
+                System.out.println(heroes.getHealth()+" "+heroes.getDamage()+" "+heroes.getQuirk());
+            }
+
+        }
+
+
+//        for (int i = 0; i <createHeroes().length; i++) {
+//            System.out.println(String.valueOf(createHeroes()));
+//               break;
+//        }
+
+
+
+        }
+
+    public static Hero[] createHeroes(){
+
+        Hero hero1=new Hero(200,15,"self-defence");
+        Hero hero2 =new Hero(200,20);
+        Hero hero3=new Hero(300,30,"enhancer");
+
+//        System.out.println(hero1.getHealth()+ "-" +hero1.getDamage()+"-"+hero1.getQuirk());
+//        System.out.println(hero2.getHealth()+ "-" +hero2.getDamage());
+//        System.out.println(hero3.getHealth()+ "-" +hero3.getDamage()+"-"+hero3.getQuirk());
+        return new Hero[]{hero1,hero2,hero3} ;
+
+
     }
+
+
+
 }
